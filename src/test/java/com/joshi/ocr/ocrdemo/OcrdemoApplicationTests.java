e8f04f41500d57ddc81d234d0ce2d2c675e3d5a7
Narayan Joshi <geevan.josh@gmail.om>
2019-01-02 23:50:35 +0545

    added dependency and added test case

--- src/test/java/com/joshi/ocr/ocrdemo/OcrdemoApplicationTests.java
+++ src/test/java/com/joshi/ocr/ocrdemo/OcrdemoApplicationTests.java
@@ -5,12 +5,26 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OcrdemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		Tesseract tesseract = new Tesseract();
+		try {
+			tesseract.setDatapath("E:/tessdata-master/tessdata-master");
+			String text = tesseract.doOCR(new File("C:/Users/Narayan Joshi/Pictures/image.png"));
+			System.out.print(text);
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
